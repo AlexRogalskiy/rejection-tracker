6db6f802a09ccd61c3b39497b97b9655a4da991c
garronej <joseph.garrone@ensimag.grenoble-inp.fr>
2017-04-11 12:21:34 +0000

    fix max listener

--- dist/lib/rejectionTracker.js
+++ dist/lib/rejectionTracker.js
@@ -2,36 +2,26 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var path = require("path");
 require("colors");
+process.setMaxListeners(Infinity);
 function rejectionTracker() {
     var projectPaths = [];
     for (var _i = 0; _i < arguments.length; _i++) {
         projectPaths[_i] = arguments[_i];
     }
     var projectPath = path.join.apply(path, projectPaths);
-    console.log("add rejection handler", projectPath);
     process.on("unhandledRejection", function (error) {
-        if (!(error instanceof Error) || !error.stack)
+        if (!(error instanceof Error))
             return;
         var split = error.stack.split("\n");
         var regExp = /^at\ (?:[^\ ]+\ \()?(.*)(?:\:[0-9]+){2}\)?$/;
         var match = split[1].trim().match(regExp);
         var filePath = match[1];
         var relativePath = path.relative(projectPath, filePath);
-        /*
-        filePath= "/home/pi/github/test.js";
-        filePath= "/home/pi/github/rejection-tracker/node_modules/m/test.js";
-
-        console.log("filePath: ",filePath);
-        console.log("projectPath: ", projectPath);
-
-        console.log("relativePath: ", relativePath);
-
-        console.log(relativePath.match(/^(?:\.\.|node_modules)/));
-        */
         if (relativePath.match(/^(?:\.\.|node_modules)/))
             return;
         console.log((projectPath.split(path.sep).pop() + " internal error").red);
-        console.log(error.stack);
+        if (error.stack)
+            console.log(error.stack);
         process.exit(-1);
     });
 }
--- src/lib/rejectionTracker.ts
+++ src/lib/rejectionTracker.ts
@@ -1,17 +1,15 @@
 import * as path from "path";
 import "colors";
 
+process.setMaxListeners(Infinity);
+
 export function rejectionTracker(...projectPaths: string[]): void {
 
-    let projectPath= path.join.apply(path, projectPaths);
-
-    console.log("add rejection handler", projectPath);
+    let projectPath = path.join.apply(path, projectPaths);
 
     process.on("unhandledRejection", error => {
 
-
-        if (!(error instanceof Error) || !error.stack )
-            return;
+        if (!(error instanceof Error)) return;
 
         let split = error.stack!.split("\n");
 
@@ -21,31 +19,16 @@ export function rejectionTracker(...projectPaths: string[]): void {
 
         let filePath = match![1];
 
+        let relativePath = path.relative(projectPath, filePath);
 
-        let relativePath= path.relative(projectPath, filePath);
-
-        /*
-        filePath= "/home/pi/github/test.js";
-        filePath= "/home/pi/github/rejection-tracker/node_modules/m/test.js";
-
-        console.log("filePath: ",filePath);
-        console.log("projectPath: ", projectPath);
-
-        console.log("relativePath: ", relativePath);
-
-        console.log(relativePath.match(/^(?:\.\.|node_modules)/));
-        */
-
-        if (relativePath.match(/^(?:\.\.|node_modules)/) )
-            return;
+        if (relativePath.match(/^(?:\.\.|node_modules)/)) return;
 
         console.log(`${projectPath.split(path.sep).pop()} internal error`.red);
 
-        console.log(error.stack);
+        if( error.stack ) console.log(error.stack);
 
         process.exit(-1);
 
     });
-
 
 }
